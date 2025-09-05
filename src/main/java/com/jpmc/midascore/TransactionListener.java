d55d399d7c414f0fb2639ae22125abec415673ed
simonefrs <simone.ferrarese.sf@gmail.com>
2025-09-05 12:39:02 +0200

    Task2: add transaction listener

---
+++ src/main/java/com/jpmc/midascore/TransactionListener.java
@@ -0,0 +1,18 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+            groupId = "midas-core"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("Received transaction: " + transaction.getAmount());
+    }
+}
+
